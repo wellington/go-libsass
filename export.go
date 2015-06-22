24a56bd59238d3f1f785ead3154991315dc0b283
Drew Wells <drew.wells00@gmail.com>
2015-06-21 19:36:31 -0500

    umm idk

--- export.go
+++ export.go
@@ -5,9 +5,9 @@ package context
 // defines additional C code necessary for the context<->sass_context bridge.
 // See: http://golang.org/cmd/cgo/#hdr-C_references_to_Go
 
-// #cgo CFLAGS: -O2 -stdlib=libc++ -fPIC
+// #cgo CFLAGS: -O2 -fPIC
 // #cgo CPPFLAGS: -w
-// #cgo CXXFLAGS: -g -std=c++0x -pedantic -Wno-c++11-extensions -O2 -std=c++0x -stdlib=libc++ -fPIC
+// #cgo CXXFLAGS: -g -std=c++0x -pedantic -Wno-c++11-extensions -O2 -fPIC
 // #include "sass_context.h"
 //
 import "C"
