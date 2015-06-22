4cdf225c9b570199942b38c82397670be237829f
Drew Wells <drew.wells00@gmail.com>
2015-06-22 01:10:50 -0500

    arch specific ldflags

--- export.go
+++ export.go
@@ -8,6 +8,7 @@ package context
 // #cgo CFLAGS: -O2 -fPIC
 // #cgo CPPFLAGS: -w
 // #cgo CXXFLAGS: -g -std=c++0x -pedantic -Wno-c++11-extensions -O2 -fPIC
+// #cgo darwin linux LDFLAGS: -ldl
 // #cgo LDFLAGS: -lstdc++ -lm
 // #include "sass_context.h"
 //
