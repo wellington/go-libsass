8b6d55e5c1f835c6c4c5801d9dea94749e99230a
Drew Wells <drew.wells00@gmail.com>
2015-07-19 23:24:30 -0500

    safe transformation?

--- libs/ast.cpp
+++ libs/ast.cpp
@@ -5,4 +5,9 @@
 #include "../libsass-build/position.cpp"
 #include "../libsass-build/constants.cpp"
 #include "../libsass-build/inspect.cpp"
+#include "../libsass-build/source_map.cpp"
+#include "../libsass-build/error_handling.cpp"
+#include "../libsass-build/to_string.cpp"
+
+#include "../libsass-build/sass_functions.cpp"
 #endif
