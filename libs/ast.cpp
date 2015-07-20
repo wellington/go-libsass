7a3c0a4b26a591ca92d9b2ec74380c065cb68cdb
Drew Wells <drew.wells00@gmail.com>
2015-07-19 23:01:15 -0500

    ast me nicely

--- libs/ast.cpp
+++ libs/ast.cpp
@@ -1,4 +1,8 @@
 #ifndef USE_LIBSASS
 #include "../libsass-build/ast.cpp"
 #include "../libsass-build/util.cpp"
+#include "../libsass-build/context.cpp"
+#include "../libsass-build/position.cpp"
+#include "../libsass-build/constants.cpp"
+#include "../libsass-build/inspect.cpp"
 #endif
