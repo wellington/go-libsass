fbc8f2bb8a2c4db50f61dc8401e9b3fce3f16651
Drew Wells <drew.wells00@gmail.com>
2015-07-30 23:44:06 -0500

    print statement

--- simple/simple.go
+++ simple/simple.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/wellington/go-libsass/libs"
+import (
+	"fmt"
+
+	"github.com/wellington/go-libsass/libs"
+)
 
 func main() {
 	godc := libs.SassMakeDataContext("div { p { color: red; } }")
 	gocompiler := libs.SassMakeDataCompiler(godc)
 	libs.SassCompilerParse(gocompiler)
+	fmt.Println("successfully executed")
 }
