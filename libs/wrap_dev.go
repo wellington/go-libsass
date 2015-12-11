20b72584c5be1aa9c107a1dae914fc8e81c3124a
Drew Wells <drew.wells00@gmail.com>
2015-12-10 22:39:05 -0600

    remove pkg-config dependency

--- libs/wrap_dev.go
+++ libs/wrap_dev.go
@@ -2,8 +2,8 @@
 
 package libs
 
-// #cgo pkg-config: libsass
 // #cgo CPPFLAGS: -DUSE_LIBSASS
+// #cgo CPPFLAGS: -I../libsass-build -I../libsass-build/include
 // #cgo LDFLAGS: -lsass
 //
 import "C"
