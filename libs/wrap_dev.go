05715ea8edfbb68f55553588f3dc20eca1815323
Drew Wells <drew.wells00@gmail.com>
2015-12-19 22:48:30 -0600

    dl is necessary for linux

--- libs/wrap_dev.go
+++ libs/wrap_dev.go
@@ -5,5 +5,6 @@ package libs
 // #cgo CPPFLAGS: -DUSE_LIBSASS
 // #cgo CPPFLAGS: -I../libsass-build -I../libsass-build/include
 // #cgo LDFLAGS: -lsass
+// #cgo darwin linux LDFLAGS: -ldl
 //
 import "C"
